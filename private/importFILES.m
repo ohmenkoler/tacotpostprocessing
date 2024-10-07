4b779714c0df0dc5392b3023060b30b03b2ac48c
ohmenkoler <martin.fontbonne@univ-poitiers.fr>
2024-10-07 13:16:41 +0200

    Update importFILES.m

--- private/importFILES.m
+++ private/importFILES.m
@@ -1,12 +1,14 @@
-function [BigChannels,BigChannelsNames,f_sampling] = importFILES
+function [BigChannels,f_sampling] = importFILES
 % This function imports 1 or more .tdms files, then extracts the 
 % Channels data and names, and the number of files imported.
 %   OUTPUTS:
 %       BigChannels: 3D-matrix of the data channels (Nsamples x 30 x Nfiles)
-%       BigChannelsNames: vector of the channels names (1 x 30)
+%       f_sampling: sampling frequency of the acquisition
 
 %% Path and name
-[filename,filepath] = uigetfile({'conv*.tdms';'*.xls*'},'MultiSelect','on');
+[filename,filepath] = uigetfile({'*.tdms';'*.xls*'},'Select a .tdms file to process',...
+    'Z:\Martin\Measurements\TACOT\V2_AddedSensors',...
+    'MultiSelect','on');
 if iscell(filename) == true                 % When N file are imported
     pathname = strcat(filepath,filename);   % they are already in a cell
 else                                        % but when 1 is imported
@@ -16,27 +18,25 @@ end                                         % be transtyped to a cell)
 Nfiles = length(pathname);
 
 %% Convert the files (first 1...
-tmp = convertTDMS(0,pathname(1));
-BigChannels = [tmp.Data.MeasuredData.Data] ;    % Initialising the BigChannels matrix 
-                                                % + getting the channels for 1 file import
-BigChannelsNames = {tmp.Data.MeasuredData.Name};
-BigChannelsNames = BigChannelsNames(3:end);
 
+tmp_info = tdmsinfo(pathname(1));
+tmp_channelGroup = tmp_info.ChannelList{1,2};
+tmp_channelName = tmp_info.ChannelList{1,4};
+
+tmp_fs = tdmsreadprop(pathname(1),ChannelGroupName=tmp_channelGroup,ChannelName=tmp_channelName);
+f_sampling = fix(1./tmp_fs{1,13}); %1651
+
+BigChannels = tdmsread(pathname(1),SampleRate=f_sampling);
+BigChannels = BigChannels{1,1};
+ 
 %% ... then the other if needed)
 if Nfiles ~= 1
     for ifile = 2:Nfiles                        % Start at 2 as first one is already treated in the init
-        tmp = convertTDMS(0,pathname(ifile));
-        Channels = [tmp.Data.MeasuredData.Data];
-%         ChannelsNames = {tmp.Data.MeasuredData.Name};
-%         ChannelsNames = ChannelsNames(3:end);
-
-        BigChannels = cat(3,BigChannels,Channels);
-%         BigChannelsNames = cat(3,BigChannelsNames,ChannelsNames);
+        tmp = tdmsread(pathname(1),SampleRate=f_sampling);
+       
+        BigChannels = cat(3,BigChannels,tmp);
     end
-end
-%% Sampling info
-T_sampling = tmp.Data.MeasuredData(3).Property(3).Value;
-f_sampling = 1 / T_sampling;
+
 
 end
 
