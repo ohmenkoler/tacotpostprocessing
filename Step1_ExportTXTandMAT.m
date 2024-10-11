ab3c4a7343c107c201dee45c83308fa1b0c54f21
ohmenkoler <martin.fontbonne@univ-poitiers.fr>
2024-10-11 16:36:06 +0200

    Refait H1H2 mid
    Coupé à 2700 s

--- Step1_ExportTXTandMAT.m
+++ Step1_ExportTXTandMAT.m
@@ -15,7 +15,7 @@ else
     SelectCHXtrig = 7;
 end
 
-T_end = 3500;                                              % Total duration of measurement in s.
+T_end = 2700;                                              % Total duration of measurement in s.
 BigData = BigData(1:Conf.Acquisition.F_sampling*T_end,:);    % Resizing to have same length for all
 
 %% Configuration of measurement
@@ -31,8 +31,8 @@ Conf.Parameters.CanisterType = 'Inox';
 Conf.Parameters.Cpwater = 4185;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Conf.Acquisition.Iteration = 3;
-Conf.Parameters.Orientation = 'V1';
+Conf.Acquisition.Iteration = 1;
+Conf.Parameters.Orientation = 'H2';
 Conf.Acquisition.ManipType = 'Acou';
 Conf.Acquisition.Acq = 'Transient';
 Conf.Acquisition.Amplitude = 'Mid';
