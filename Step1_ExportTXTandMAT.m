1c5b02b1c2d7c094bd02e1efb621f4608628bc1b
ohmenkoler <martin.fontbonne@univ-poitiers.fr>
2024-10-24 22:01:27 +0200

    Update Step1_ExportTXTandMAT.m

--- Step1_ExportTXTandMAT.m
+++ Step1_ExportTXTandMAT.m
@@ -32,10 +32,10 @@ Conf.Parameters.Cpwater = 4185;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Conf.Acquisition.Iteration = 2;
-Conf.Parameters.Orientation = 'V1';
-Conf.Acquisition.ManipType = 'Acou';
+Conf.Parameters.Orientation = 'H2';
+Conf.Acquisition.ManipType = 'HeatOnly';
 Conf.Acquisition.Acq = 'Transient';
-Conf.Acquisition.Amplitude = 'High';
+Conf.Acquisition.Amplitude = 'Off';
 Conf.Parameters.UCHX_Vrms = 0;
 switch Conf.Acquisition.Amplitude
     case {'Low','Lo','low','lo'}
@@ -67,6 +67,7 @@ DPS = BigData(end-N_end:end,SelectDPS);
 UIRix = BigData(end-N_end:end,SelectUIRix);
 
 CHXTrig = BigData(:,SelectCHXtrig);
+CHXDutyCycle = mean(CHXTrig);
 
 TC = BigData(:,SelectTC);
 TA_core = TC(:,2:end);
